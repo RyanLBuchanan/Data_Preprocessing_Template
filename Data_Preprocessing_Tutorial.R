2e9967519bbce507d280b2ecf2dae9d7281713d1
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-06 17:30:19 -0600

    R!

--- Data_Preprocessing_Tutorial.R
+++ Data_Preprocessing_Tutorial.R
@@ -1,60 +1,20 @@
 # Data Prepocessing Template from Machine Learning A-Z - SuperDataScience
-# Input by Ryan L Buchanan 31AUG20
-
-# Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+# Input by Ryan L Buchanan 06SEP20
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-
-print(X)
-print(y)
+dataset = read.csv('Data.csv')
 
 # Taking care of the missing data
-from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
-print(X)
+data$Age = ifelse(is.na(dataset$Age),
+                  ave(dataset$Age, FUN = function(x) mean(x, na.rm = True)),
+                  dataset$Age)
 
 # Encoding categorical data
 # Encoding the Independent Variable
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
-X = np.array(ct.fit_transform(X))
-
-print(X)
 
 # Encoding the Dependent Variable
-from sklearn.preprocessing import LabelEncoder
-le = LabelEncoder()
-y = le.fit_transform(y)
-
-print(y)
 
 # Splitting the dataset into the Training set and the Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=1)
 
-print(X_train)
-
-print(X_test)
-
-print(y_train)
-
-print(y_test)
 
 # Feature Scaling pplied after training to avoid info leakage - must be new data
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
-X_test[:, 3:] = sc.transform(X_test[:, 3:])
-
-print(X_train)
-
-print(X_test)
